7ec9c50cdfda42c99ee1cc23dc8add0d71c52c35
MAK <denbor140@gmail.com>
2026-05-11 17:14:50 +0300

    add sprite.svg and glodal.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css';
 const manrope = Manrope({
   variable: '--font-family',
   subsets: ['latin'],
-  weight: ['500', '600', '700'],
+  weight: ['400', '500', '600', '700'],
   display: 'swap',
 });
 
